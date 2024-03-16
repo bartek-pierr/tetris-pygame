b4b91aea39feadec61aae5b64e1c2bf524ae9c53
Bartek Frątczak <bartek.fratczak@akcept.eu>
2024-03-16 23:31:02 +0100

    Create user input.

--- code/game.py
+++ code/game.py
@@ -23,7 +23,8 @@ class Game:
 
         # TIMER
         self.timers = {
-            'vertical move': Timer(UPDATE_START_SPEED, True, self.move_down)
+            'vertical move': Timer(UPDATE_START_SPEED, True, self.move_down),
+            'horizontal move': Timer(MOVE_WAIT_TIME)
         }
         self.timers['vertical move'].activate()
 
@@ -46,9 +47,21 @@ class Game:
 
         self.surface.blit(self.line_surface, (0, 0))
 
+    def input(self):
+        keys = pygame.key.get_pressed()
+
+        if not self.timers['horizontal move'].active:
+            if keys[pygame.K_LEFT]:
+                self.tetromino.move_horizontal(-1)
+                self.timers['horizontal move'].activate()
+            if keys[pygame.K_RIGHT]:
+                self.tetromino.move_horizontal(1)
+                self.timers['horizontal move'].activate()
+
     def run(self):
 
         # AKTUALIZACJE
+        self.input()
         self.timer_update()
         self.sprites.update()
 
@@ -71,9 +84,14 @@ class Tetromino:
         # TWORZENIE BLOKÓW
         self.blocks = [Block(group, pos, self.color) for pos in self.block_positions]
 
+    def move_horizontal(self, amount):
+        for block in self.blocks:
+            block.pos.x += amount
+
     def move_down(self):
         for block in self.blocks:
             block.pos.y += 1
+
 class Block(pygame.sprite.Sprite):
     def __init__(self, group, pos, color):
 
