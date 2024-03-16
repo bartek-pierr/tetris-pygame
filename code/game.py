5ecd32ec3bacbd7c41db9572d2e98fff4693bd92
Bartek Frątczak <bartek.fratczak@akcept.eu>
2024-03-16 21:19:46 +0100

    Making random shape on start of the game.

--- code/game.py
+++ code/game.py
@@ -1,4 +1,5 @@
 from settings import *
+from random import choice
 
 class Game:
     def __init__(self):
@@ -7,12 +8,16 @@ class Game:
         self.surface = pygame.Surface((GAME_WIDTH, GAME_HEIGHT))
         self.display_surface = pygame.display.get_surface()
         self.rect = self.surface.get_rect(topleft = (PADDING, PADDING))
+        self.sprites = pygame.sprite.Group()
 
         # LINIE
         self.line_surface = self.surface.copy()
         self.line_surface.fill((0, 255, 0))
         self.line_surface.set_colorkey((0, 255, 0))
         self.line_surface.set_alpha(120)
+
+        # TETROMINO
+        self.tetromino = Tetromino(choice(list(TETROMINOS.keys())), self.sprites)
 
     def draw_grid(self):
 
@@ -30,7 +35,33 @@ class Game:
 
         # RYSOWANIE
         self.surface.fill(GRAY)
+        self.sprites.draw(self.surface)
 
         self.draw_grid()
         self.display_surface.blit(self.surface, (PADDING, PADDING))
         pygame.draw.rect(self.display_surface, LINE_COLOR, self.rect, 2, 2)
+        
+
+class Tetromino:
+    def __init__(self, shape, group):
+
+        # KONFIGURACJA
+        self.block_positions = TETROMINOS[shape]['shape']
+        self.color = TETROMINOS[shape]['color']
+
+        # TWORZENIE BLOKÓW
+        self.blocks = [Block(group, pos, self.color) for pos in self.block_positions]
+
+class Block(pygame.sprite.Sprite):
+    def __init__(self, group, pos, color):
+
+        # OGÓLNE
+        super().__init__(group)
+        self.image = pygame.Surface((CELL_SIZE, CELL_SIZE))
+        self.image.fill(color)
+
+        # POZYCJA
+        self.pos = pygame.Vector2(pos) + BLOCK_OFFSET
+        x = self.pos.x * CELL_SIZE
+        y = self.pos.y * CELL_SIZE
+        self.rect = self.image.get_rect(topleft = (x, y))
--- code/settings.py
+++ code/settings.py
@@ -1,4 +1,5 @@
 import pygame
+from pygame.sprite import Sprite, Group
 
 # WYMIARY GRY
 COLUMNS = 10
@@ -32,3 +33,16 @@ CYAN = '#6cc6d9'
 ORANGE = '#f07e13'
 GRAY = '#1c1c1c'
 LINE_COLOR = '#ffffff'
+
+# KSZTAŁTY
+TETROMINOS = {
+    'T': {'shape': [(0, 0), (-1, 0), (1, 0), (0, -1)], 'color': PURPLE},
+    'O': {'shape': [(0, 0), (0, -1), (1, 0), (1, -1)], 'color': YELLOW},
+    'J': {'shape': [(0, 0), (0, -1), (0, 1), (-1, 1)], 'color': BLUE},
+    'L': {'shape': [(0, 0), (0, -1), (0, 1), (1, 1)], 'color': ORANGE},
+    'I': {'shape': [(0, 0), (0, -1), (0, -2), (0, 1)], 'color': CYAN},
+    'S': {'shape': [(0, 0), (-1, 0), (0, -1), (1, -1)], 'color': GREEN},
+    'Z': {'shape': [(0, 0), (1, 0), (0, -1), (-1, -1)], 'color': RED}
+}
+
+SCORE_DATA = {1: 40, 2: 100, 3: 300, 4: 1200}
